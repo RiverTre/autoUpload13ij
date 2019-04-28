8a68860f8a8c3c6f2796f0280a38f3ecbd09e3ec
John <39196446+RiverTre@users.noreply.github.com>
2019-04-29 00:18:57 +0800

    实例代码
    使用python打开excel以及主程序代码示例

---
+++ openExcel.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import  xdrlib ,sys
+import xlrd
+
+# try:
+data = xlrd.open_workbook('E:\\file.xlsx')
+table = data.sheets()[0]
+nrows = table.nrows #行数
+ncols = table.ncols #列数
+# 第二行
+cell_A1 = table.col(0)[1].value# 第1列
+cell_B1 = table.col(1)[1].value# 第2列
+cell_C1 = table.col(2)[1].value# 第3列
+print(table.row_values(0))
+print('A1',cell_A1)
+print('B1',cell_B1)
+print('C1',cell_C1)
+print(ncols)
